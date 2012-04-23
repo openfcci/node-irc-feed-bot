6dd3c66dda8ba70466f0ccd5c87f8a6f6f2df3b6
Zac Echola <zac.echola@gmail.com>
2012-04-23 16:50:11 -0500

    send commands to the robot via private messages.

--- notify.js
+++ notify.js
@@ -4,28 +4,48 @@ var IRC = require('irc');
 
 // Set up your little spambot
 var server    = 'sobby.fccinteractive.com';
-var bot       = 'noder';
-var channel   = ['#testing', '#testing2'];
+var bot       = 'feed-bot';
+var channels   = ['#testing', '#testing2'];
 var feed      = 'http://rss.cnn.com/rss/cnn_latest.rss';
 var interval  = 1 // how often to poll the feed?
 
 
 client = new IRC.Client(server, bot,
                         {
-                          channels: channel 
+                          channels: channels 
                         });
 
 reader = new FEEDSUB(feed, {
   interval: interval,
   autoStart: true,
-  emitOnStart: true
 });
 
 reader.on('item', function(item) {
-  client.say(channel, item.title);
+  var first
+  client.say(channels, item.title + ': ' + item.link);
   console.dir(item.title);
 });
 
 client.addListener('pm', function(from, message) {
   console.log('PM from %s => %s', from, message);
+
+  if (message.match(/die/i)) {
+    client.part(channels)
+  }
+  if (message.match(/quiet/i)) {
+      reader.interval = null;
+  }
+  if (message.match(/noisy/i)) {
+      reader.interval = 1;
+  }
+  if (message.match(/join #+[a-zA-z0-9]+$/i)) {
+    var channel = message.match(/#+[a-zA-z0-9]+$/).toString();
+    console.log('Joining ' + channel);
+    client.join(channel);
+  }
+  if (message.match(/part #+[a-zA-z0-9]+$/i)) {
+    var channel = message.match(/#+[a-zA-z0-9]+$/).toString();
+    console.log('Parting ' + channel);
+    client.part(channel);
+  }
 });
