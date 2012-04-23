4a24529a6abd87d7b7e308a14eeb72af5ff80829
Zac Echola <zac.echola@gmail.com>
2012-04-23 14:39:04 -0500

    a little reorg

--- notify.js
+++ notify.js
@@ -1,19 +1,31 @@
 var FEEDSUB = require('feedsub');
 var IRC = require('irc');
 
-client = new IRC.Client('irc.example.com', 
-                        'Feed-bot', // Name your bot 
-                        {
-                        channels: ['#testing', '#the-serious-room'],
-});
 
-reader = new FEEDSUB('http://example.com/atom.xml', {
-  interval: 10, // check feed every ten minutes
+// Set up your little spambot
+var server    = 'sobby.fccinteractive.com';
+var bot       = 'noder';
+var channel   = ['#testing', '#testing2'];
+var feed      = 'http://rss.cnn.com/rss/cnn_latest.rss';
+var interval  = 1 // how often to poll the feed?
+
+
+client = new IRC.Client(server, bot,
+                        {
+                          channels: channel 
+                        });
+
+reader = new FEEDSUB(feed, {
+  interval: interval,
   autoStart: true,
+  emitOnStart: true
 });
 
 reader.on('item', function(item) {
+  client.say(channel, item.title);
   console.dir(item.title);
-  client.say(client.channels, item.title + ': ' + item.id);
 });
 
+client.addListener('pm', function(from, message) {
+  console.log('PM from %s => %s', from, message);
+});
